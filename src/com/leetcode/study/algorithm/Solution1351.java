5542769dd3388266af6efe2c8e6337e08c3c8235
yanlp <85827444@qq.com>
2020-12-28 12:45:38 +0800

    1351. 统计有序矩阵中的负数

---
+++ src/com/leetcode/study/algorithm/Solution1351.java
@@ -0,0 +1,63 @@
+package com.leetcode.study.algorithm;
+
+/**
+ * 1351. 统计有序矩阵中的负数
+ * 给你一个 m * n 的矩阵 grid，矩阵中的元素无论是按行还是按列，都以非递增顺序排列。 
+ * <p>
+ * 请你统计并返回 grid 中 负数 的数目。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+ * 输出：8
+ * 解释：矩阵中共有 8 个负数。
+ * 示例 2：
+ * <p>
+ * 输入：grid = [[3,2],[1,0]]
+ * 输出：0
+ * 示例 3：
+ * <p>
+ * 输入：grid = [[1,-1],[-1,-1]]
+ * 输出：3
+ * 示例 4：
+ * <p>
+ * 输入：grid = [[-1]]
+ * 输出：1
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 100
+ * -100 <= grid[i][j] <= 100
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/count-negative-numbers-in-a-sorted-matrix
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * Program Name: leetcode-study
+ * Created by yanlp on 2020-12-28
+ *
+ * @author yanlp
+ * @version 1.0
+ */
+public class Solution1351 {
+    public  static int countNegatives(int[][] grid) {
+        int count = 0;
+        for (int[] outIntArr : grid) {
+            for (int innerInt : outIntArr) {
+                if (innerInt < 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[][] a = {{1,2,-3,-3},{3,4}};
+        System.out.println(countNegatives(a));
+    }
+}
